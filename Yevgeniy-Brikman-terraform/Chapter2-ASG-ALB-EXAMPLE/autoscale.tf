d509f0a51b777ed93e1f4b3f7306cf1dcea2728c
rvadisala <ravi.vadisala@yahoo.co.in>
2021-07-19 14:03:03 +0530

    Add files via upload

---
+++ Yevgeniy-Brikman-terraform/Chapter2-ASG-ALB-EXAMPLE/autoscale.tf
@@ -0,0 +1,26 @@
+resource "aws_autoscaling_group" "web_asg" {
+  name                  = "asg-webserver"
+  launch_configuration  = aws_launch_configuration.apache_web.id
+  capacity_rebalance    = true
+  default_cooldown      = "300"
+  desired_capacity      = "1"
+  force_delete          = true
+  max_instance_lifetime = 0
+  max_size              = 2
+  min_size              = 2
+  vpc_zone_identifier   = [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]]
+  target_group_arns     = [aws_lb_target_group.web_target_group.arn]
+  health_check_type     = "ELB"
+  tag {
+    key                 = "Name"
+    value               = "webserver_autoscale"
+    propagate_at_launch = true
+  }
+  instance_refresh {
+    strategy = "Rolling"
+    preferences {
+      min_healthy_percentage = 50
+    }
+    triggers = ["tag"]
+  }
+}
---
+++ Yevgeniy-Brikman-terraform/Chapter2-ASG-ALB-EXAMPLE/aws_lb.tf
@@ -0,0 +1,86 @@
+resource "aws_lb" "web_lb" {
+  name                             = "netenrich-alb"
+  internal                         = false
+  load_balancer_type               = "application"
+  subnets                          = [module.vpc.public_subnets[0], module.vpc.public_subnets[1], module.vpc.public_subnets[2]]
+  enable_deletion_protection       = false
+  drop_invalid_header_fields       = true
+  enable_cross_zone_load_balancing = true
+  security_groups                  = [aws_security_group.alb_web.id]
+
+}
+
+resource "aws_lb_listener" "web_listener" {
+  load_balancer_arn = aws_lb.web_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "web_listener_rule" {
+  listener_arn = aws_lb_listener.web_listener.arn
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_target_group.arn
+  }
+
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+}
+
+
+resource "aws_lb_target_group" "web_target_group" {
+  name     = "apache-web-tgt-grp"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+
+
+
+resource "aws_security_group" "alb_web" {
+
+  name        = "web_alb_traffic"
+  description = "Allow web traffic"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    protocol    = "tcp"
+    to_port     = 80
+  }
+
+  egress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+  }
+}
